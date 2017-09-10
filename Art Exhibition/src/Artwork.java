1ac3d8036813241307674ee53bbc01df5d3c7b1f
LachlanRMIT <30454053+LachlanRMIT@users.noreply.github.com>
2017-09-11 08:29:18 +1000

    fields added

--- Art Exhibition/src/Artwork.java
+++ Art Exhibition/src/Artwork.java
@@ -1,4 +1,16 @@
 
 public class Artwork {
-
+	
+	public static String artName;
+	public static String artist;
+	//public static String locaction;
+	public static double price;
+	public static boolean forSale;
+	
+	public Artwork(String artName, String artist,double price,boolean forSale) {
+		this.artName 	= artName;
+		this.artist		= artist;
+		this.price		= price;
+		this.forSale	= forSale;
+	}
 }
