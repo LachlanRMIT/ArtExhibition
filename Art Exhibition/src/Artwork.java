379ce474dd1a059c7032e6cdce2bb4e5d533add3
LachlanRMIT <30454053+LachlanRMIT@users.noreply.github.com>
2017-09-11 08:49:15 +1000

    accessor methods created

--- Art Exhibition/src/Artwork.java
+++ Art Exhibition/src/Artwork.java
@@ -1,17 +1,41 @@
 
 public class Artwork {
 	
-	public static String artName;
+	public static String title;
 	public static String artist;
 	//public static String location
 	//public static String image
 	public static double price;
 	public static boolean forSale;
 	
-	public Artwork(String artName, String artist,double price,boolean forSale) {
-		this.artName 	= artName;
+	public Artwork(String title, String artist,double price,boolean forSale) {
+		this.title 		= title;
 		this.artist		= artist;
 		this.price		= price;
 		this.forSale	= forSale;
+	}
+	
+	//get the title of the artwork
+	public static String getTitle() {
+		return title;
+	}
+	
+	//get the artist of the artwork
+	public static String getArtist() {
+		return artist;
+	}
+	
+	//get the location of the artwork
+	
+	//get the image of the artwork
+	
+	//get the price of the artwork
+	public static double getPrice() {
+		return price;
+	}
+	
+	//check is the artwork is for sale of not
+	public static boolean forSale() {
+		return forSale;
 	}
 }
